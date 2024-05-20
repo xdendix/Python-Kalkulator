670b98402750c1f7686b1ca922d8d836d01ab54a
xdendix <dendiprayogo24@gmail.com>
2024-05-20 20:31:32 +0700

    Menambahkan Operator Modulo

--- python-kalkulator/main.py
+++ python-kalkulator/main.py
@@ -3,7 +3,7 @@ print("Python Kalkulator")
 print(17 * "=")
 
 angka_1 = float(input("Masukkan Angka 1: "))
-operator = input("Masukkan Operator (+, -, x, :): ")
+operator = input("Masukkan Operator (+, -, x, :, %): ")
 angka_2 = float(input("Masukkan Angka 2: "))
 
 if operator == "+":
@@ -18,5 +18,9 @@ elif operator == "x" or operator == "*":
 elif operator == ":" or operator == "/":
     hasil = angka_1 / angka_2
     print(f"Hasilnya adalah = {hasil}")
+elif operator == "%":
+    hasil = angka_1 % angka_2
+    print(f"Hasilnya adalah = {hasil}")
+    
 else:
     print("Hanya masukkan angka!")
